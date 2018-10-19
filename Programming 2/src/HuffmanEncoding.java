80212ca95852790caa4586773c112af5038f0fc0
Jake Wiseberg <jakewiseberg@Jakes-Sidekick.local>
2018-10-19 13:31:00 -0400

    progress on P2

--- Programming 2/src/HuffmanEncoding.java
+++ Programming 2/src/HuffmanEncoding.java
@@ -10,57 +10,86 @@ import java.io.*;
 
 
 class HuffmanNode {
-  private Character inChar;
-  private int frequency;
-  private HuffmanNode left, right;
-  
-  public HuffmanNode(Character inChar, int frequency, HuffmanNode left, HuffmanNode right) {
-    this.inChar = inChar;
-    this.frequency = frequency;
-    this.left = left;
-    this.right = right;
-  }
-  
-  
+    private Character inChar;
+    public int frequency;
+    public HuffmanNode left, right;
+
+    public HuffmanNode(Character inChar, int frequency, HuffmanNode left, HuffmanNode right) {
+        this.inChar = inChar;
+        this.frequency = frequency;
+        this.left = left;
+        this.right = right;
+    }
+
+    public Character getInChar() { return inChar; }
+
+
 }
 
 public class HuffmanEncoding {
-  
-  public static void main(String[] args) {
-    FileReader in = null;
-    FileWriter out = null;
-    try {
-      in = new FileReader("input.txt");
-      out = new FileWriter("output.txt");
-      int c;
-      while ((c = in.read()) != -1) {
-        out.write(c);
-      }
-    } finally {
-      if (in != null)
-        in.close();
-      if (out != null)
-        out.close();
+    public static void main(String[] args) {
+        huffmanCoder("input.txt", "output.txt");
     }
-  }
-  
-  /*
-  public static String huffmanCoder(​String​ inputFileName​,​ ​String​ outputFileName​) {
-    return "";
-  }
-  
-  public static HuffmanNode textReader(String inputFileName) {
-    
-  }
-  
-  public static HuffmanNode mergeNodes(HuffmanNode n) {
-    
-  }
-  
-  public static HuffmanNode buildTree() {
-    
-  }
-  */
+
+    public static String huffmanCoder(​String inputFileName​,​ ​String outputFileName​) {
+        try {
+            File in = new File(inputFileName​);
+            FileWriter out = new FileWriter(outputFileName​);
+
+            //Array of all the characters from the input file
+            char[] input = textReader(in);
+
+            //256 is the length of the ASCII alphabet so that's as long as the frequency array should be
+            int[] freq = new int[256];
+            for (int i = 0; i < input.length; i++)
+                freq[input[i]]++;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return outputFileName​;
+    }
+
+    //This method returns an array of chars that can be analyzed and broken into HuffmanNodes
+    //Used a string then broke it into an array so I wouldn't have to deal with initial char array size
+    private static char[] textReader(File f) {
+        FileInputStream fis = null;
+        String str = "";
+
+        try {
+            fis = new FileInputStream(f);
+            int content;
+            while ((content = fis.read()) != -1) {
+                //convert to char and add it to the string
+                str += (char)content;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fis != null)
+                    fis.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        return str.toCharArray();
+    }
+
+    //This method uses the frequency array to create the HuffmanNode elements and build the tree
+    private static HuffmanNode buildTree(int[] freq) {
+        for (int i=0; i<freq; i++)
+
+    }
+
+    private static HuffmanNode mergeNodes(HuffmanNode n) {}
+
+
+
   
 }
 
